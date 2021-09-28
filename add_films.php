eddc1d675c622d1587e31a16a08ca35421fb71f1
Katrin Silde <gitkatsilde@users.noreply.github.com>
2021-09-28 08:16:41 +0300

    neljas tund kodune

--- add_films.php
+++ add_films.php
@@ -10,8 +10,33 @@
 			$film_store_notice = store_film($_POST["title_input"], $_POST["year_input"], $_POST["duration_input"], $_POST["genre_input"], $_POST["studio_input"], $_POST["director_input"]);
 		}else{
 			$film_store_notice = "osa andmeid on puudu!";
+			if(empty($_POST["title_input"])){
+				$film_store_notice_title="andmed puudu";
+			}else{
+				$film_store_notice_title="";
+			}if(empty($_POST["genre_input"])){
+				$film_store_notice_genre="andmed puudu";
+			}else{
+				$film_store_notice_genre="";
+			}if(empty($_POST["studio_input"])){
+				$film_store_notice_studio="andmed puudu";
+			}else{
+				$film_store_notice_studio="";
+			}if(empty($_POST["director_input"])){
+				$film_store_notice_director="andmed puudu";
+			}else{
+				$film_store_notice_director="";
+			}
+		//välja peal kuvatava teksti värk
+		/*}if(!empty($_POST["title_input"])){
+			$title_input_name = $_POST["title_input"];
+		} else {
+			$title_input_name = "pealkiri";
+		}*/
 		}
 	}
+	
+
 ?>
 <!DOCTYPE html>
 <html lang="et">
@@ -28,7 +53,8 @@
 	<h2>Eesti filmid</h2>
 	<form method="POST">
 		<label for="title_input">Filmi pealkiri: </label>
-		<input type="text" name="title_input" id="title_input" placeholder="pealkiri">
+		<input type="text" name="title_input" id="title_input" placeholder="pealkiri" value="<?php echo isset($_POST["title_input"]) ? $_POST["title_input"] : ''; ?>">
+		<label for="title_input"> <?php echo $film_store_notice_title;?> </label>
 		<br>
 		<label for="year_input">Valmimis aasta: </label>
 		<input type="number" name="year_input" id="year_input" value="<?php echo date("Y"); ?>" min="1912">
@@ -37,13 +63,16 @@
 		<input type="number" name="duration_input" id="duration_input" value="60" min="1">
 		<br>
 		<label for="genre_input">Filmi žanr: </label>
-		<input type="text" name="genre_input" id="genre_input" placeholder="žanr">
+		<input type="text" name="genre_input" id="genre_input" placeholder="žanr" value="<?php echo isset($_POST["genre_input"]) ? $_POST["genre_input"] : ''; ?>">
+		<label for="genre_input"> <?php echo $film_store_notice_genre;?> </label>
 		<br>
 		<label for="studio_input">Filmi tootja: </label>
-		<input type="text" name="studio_input" id="studio_input" placeholder="tootja">
+		<input type="text" name="studio_input" id="studio_input" placeholder="tootja" value="<?php echo isset($_POST["studio_input"]) ? $_POST["studio_input"] : ''; ?>">
+		<label for="studio_input"> <?php echo $film_store_notice_studio;?> </label>
 		<br>
 		<label for="director_input">Filmi lavastaja: </label>
-		<input type="text" name="director_input" id="director_input" placeholder="lavastaja">
+		<input type="text" name="director_input" id="director_input" placeholder="lavastaja" value="<?php echo isset($_POST["director_input"]) ? $_POST["director_input"] : ''; ?>">
+		<label for="director_input"> <?php echo $film_store_notice_director;?> </label>
 		<br>
 		<input type="submit" name="film_submit" value="salvesta">
 	</form>
